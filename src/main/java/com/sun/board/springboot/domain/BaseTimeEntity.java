8e879744220ee2bd6de81fc7fc68ff2851e41d43
isun-dev <florachoi1124@naver.com>
2021-04-04 15:54:18 +0900

    JPA AUDITING 추가 및 테스트 코드 작성

---
+++ src/main/java/com/sun/board/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.sun.board.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+// JPA Auditing - 수정시간 생성시간 자동화
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
