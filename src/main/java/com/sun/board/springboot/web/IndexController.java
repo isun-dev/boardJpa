3bd7ad1dff577e3aa3f23b9667bb17ac5eba6bf7
isun-dev <florachoi1124@naver.com>
2021-04-05 20:22:43 +0900

    게시글 수정 화면 및 로직 구현.

--- src/main/java/com/sun/board/springboot/web/IndexController.java
+++ src/main/java/com/sun/board/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.sun.board.springboot.web;
 
 import com.sun.board.springboot.service.PostsService;
+import com.sun.board.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,8 +20,17 @@ public class IndexController {
         return "index";
     }
 
+    // 게시글 등록
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
